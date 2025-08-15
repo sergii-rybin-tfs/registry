1570c27b77090648c3001cd1c622d258198dc0a9
Sergii <serega.rubin@gmail.com>
2025-08-15 19:20:05 +0300

    Add overloaded library

---
+++ ports/overloaded/portfile.cmake
@@ -0,0 +1,11 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO sergii-rybin-tfs/overloaded.git
+#    REF "v1.2.3" # Can be a tag, branch, or commit hash
+#    SHA512 "..." # The checksum of the downloaded file. Set to 0 to have vcpkg calculate it for you on the first run.
+    HEAD_REF master
+)
+
+file(INSTALL "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+file(INSTALL "${SOURCE_PATH}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" )
