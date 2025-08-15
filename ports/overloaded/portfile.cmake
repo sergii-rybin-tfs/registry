5dc597c91e497802a0412c23e1a422a9a707ec2d
Sergii <serega.rubin@gmail.com>
2025-08-15 20:14:55 +0300

    Update URL

--- ports/overloaded/portfile.cmake
+++ ports/overloaded/portfile.cmake
@@ -3,7 +3,7 @@ set(VCPKG_USE_HEAD_VERSION ON)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO sergii-rybin-tfs/overloaded.git
+    REPO sergii-rybin-tfs/overloaded
 #    REF "v1.2.3" # Can be a tag, branch, or commit hash
 #    SHA512 "..." # The checksum of the downloaded file. Set to 0 to have vcpkg calculate it for you on the first run.
     HEAD_REF master
